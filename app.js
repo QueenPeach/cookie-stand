c648539adf993fae62a0657d3e294a89cb1a6ee5
Ashley Jordan <ajordanium@gmail.com>
2017-11-13 15:35:50 -0800

    added stores, min and max cust, and average sales in object properties

---
+++ app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var hoursOp = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+var storeOne = {
+  name: '1st and Pike',
+  minCust: 23,
+  maxCust: 65,
+  avgSale: 6.3,
+};
+
+var storeTwo = {
+  name: 'SeaTac Airport',
+  minCust: 3,
+  maxCust: 24,
+  avgSale: 1.2,
+};
+
+var storeThree = {
+  name: 'Seattle Center',
+  minCust: 11,
+  maxCust: 38,
+  avgSale: 3.7,
+};
+
+var storeFour = {
+  name: 'Capitol Hill',
+  minCust: 20,
+  maxCust: 38,
+  avgSale: 2.3,
+};
+
+var storeFour = {
+  name: 'Alki',
+  minCust: 2,
+  maxCust: 16,
+  avgSale: 4.6,
+};
